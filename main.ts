7b58b4ca0a0b425fb92fe6e8e49f163868c909b6
DaxtonLovell <112964339+DaxtonLovell@users.noreply.github.com>
2024-10-07 12:25:13 -0600

    Pseudocode thing

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
-if (Environment.ReadNoise(AnalogPin.P1) > 5) {
-    music.play(music.stringPlayable("C - D C E C - D ", 275), music.PlaybackMode.LoopingInBackground)
-}
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("I am a cute microbit")
 })
